55e60f082cbe028f886d34217d2e28c3b2751ff0
fastturtle <gabriel_barth-maron@alumni.brown.edu>
2014-03-27 11:38:17 -0400

    added documentation

--- lbp.go
+++ lbp.go
@@ -1,7 +1,9 @@
+// This is an implementation of VLFeat's local binary pattern library
+// written in Go. It implements the local binary patterns algorithm
+// described by (Ojala, Pietikainen, & Harwood, 1996)
 package lbp
 
 import (
-	"log"
 	"math"
 	"os"
 
@@ -11,12 +13,20 @@ import (
 	_ "image/png"
 )
 
+// The Lbp struct holds information relevant to the extraction
+// of local binary patterns.
+//
+// The dim field stores the dimension (radius) of the LBP  operator
+// the mapping field is used to reduce the dimensionality of the
+// output of the original LBP operator as per (Maturana, Mery, & Soto, 2009).
+// Currently only "uniform patterns" are supported.
 type Lbp struct {
-	image   []uint8
 	dim     int
 	mapping []uint8
 }
 
+// Initializer function for creating a new Lbp struct. It returns a pointer
+// to the newly created struct and fills in its fields appropriately.
 func NewLbp() *Lbp {
 	lbp := new(Lbp)
 	lbp.dim = 58
@@ -43,58 +53,62 @@ func NewLbp() *Lbp {
 	return lbp
 }
 
-func (lbp *Lbp) preprocess(path string) (image.Image, error) {
+// All preprocessing required before running the LBP operator takes place here.
+// Currently all this does is opens the image and converts it to grayscale.
+func (lbp *Lbp) preprocess(path string) ([]uint8, image.Rectangle, error) {
 	r, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, image.Rectangle{}, err
 	}
 	defer r.Close()
 
 	im, _, err := image.Decode(r)
 	if err != nil {
-		return nil, err
+		return nil, image.Rectangle{}, err
 	}
 
 	bounds := im.Bounds()
-	lbp.image = make([]uint8, bounds.Max.Y*bounds.Max.X)
+	grayIm := make([]uint8, bounds.Max.Y*bounds.Max.X)
 
 	// Store the image as a Y * X uint8 array (grayscale)
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			gray := color.GrayModel.Convert(im.At(x, y)).(color.Gray)
-			lbp.image[x+y*bounds.Max.X] = gray.Y
+			grayIm[x+y*bounds.Max.X] = gray.Y
 		}
 	}
 
-	return im, nil
+	return grayIm, bounds, err
 }
 
-func (lbp *Lbp) at(x int, y int, w int) uint8 {
-	return lbp.image[x+y*w]
-}
-
-func (lbp *Lbp) Process(path string, regionSize int) []float64 {
-	im, err := lbp.preprocess(path)
+// The heart of the LBP algorithm. The image is treated as being composed of
+// distinct regions each of size regionSize x regionSize. The LBP operator is
+// run over the entire image, and a histogram of its (quantized) values is
+// stored for each region. These histograms are stored continguously in the
+// only return value of this function.
+func (lbp *Lbp) Process(path string, regionSize int) ([]float64, error) {
+	im, bounds, err := lbp.preprocess(path)
 	if err != nil {
-		log.Fatal(err)
-		return nil
+		return nil, err
 	}
 
-	bounds := im.Bounds()
-	rwidth := bounds.Max.X / regionSize
-	rheight := bounds.Max.Y / regionSize
+	maxX := bounds.Max.X
+	maxY := bounds.Max.Y
+
+	rwidth := maxX / regionSize
+	rheight := maxY / regionSize
 	rstride := rwidth * rheight
 
 	features := make([]float64, lbp.dim*rstride)
 
 	var bits uint8
 	var wy1, wy2, wx1, wx2 float64
-	maxX := bounds.Max.X
-	for y := 1; y < bounds.Max.Y-1; y++ {
+	var ry1, ry2, rx1, rx2 int
+	for y := 1; y < maxY-1; y++ {
 
 		wy1 = (float64(y)+0.5)/float64(regionSize) - 0.5
-		ry1 := int(math.Floor(wy1))
-		ry2 := ry1 + 1
+		ry1 = int(math.Floor(wy1))
+		ry2 = ry1 + 1
 		wy2 = wy1 - float64(ry1)
 		wy1 = 1.0 - wy2
 
@@ -102,10 +116,10 @@ func (lbp *Lbp) Process(path string, regionSize int) []float64 {
 			continue
 		}
 
-		for x := 1; x < bounds.Max.X-1; x++ {
+		for x := 1; x < maxX-1; x++ {
 			wx1 = (float64(x)+0.5)/float64(regionSize) - 0.5
-			rx1 := int(math.Floor(wx1))
-			rx2 := rx1 + 1
+			rx1 = int(math.Floor(wx1))
+			rx2 = rx1 + 1
 			wx2 = wx1 - float64(rx1)
 			wx1 = 1.0 - wx2
 
@@ -114,29 +128,29 @@ func (lbp *Lbp) Process(path string, regionSize int) []float64 {
 			}
 
 			bits = 0
-			center := lbp.at(x, y, maxX)
-			if lbp.at(x+1, y, maxX) > center {
+			center := at(im, x, y, maxX)
+			if at(im, x+1, y, maxX) > center {
 				bits |= 0x1 << 0 // E
 			}
-			if lbp.at(x+1, y+1, maxX) > center {
+			if at(im, x+1, y+1, maxX) > center {
 				bits |= 0x1 << 1 // SE
 			}
-			if lbp.at(x+0, y+1, maxX) > center {
+			if at(im, x+0, y+1, maxX) > center {
 				bits |= 0x1 << 2 // S
 			}
-			if lbp.at(x-1, y+1, maxX) > center {
+			if at(im, x-1, y+1, maxX) > center {
 				bits |= 0x1 << 3 // SW
 			}
-			if lbp.at(x-1, y+0, maxX) > center {
+			if at(im, x-1, y+0, maxX) > center {
 				bits |= 0x1 << 4 // W
 			}
-			if lbp.at(x-1, y-1, maxX) > center {
+			if at(im, x-1, y-1, maxX) > center {
 				bits |= 0x1 << 5 // NW
 			}
-			if lbp.at(x+0, y-1, maxX) > center {
+			if at(im, x+0, y-1, maxX) > center {
 				bits |= 0x1 << 6 // N
 			}
-			if lbp.at(x+1, y-1, maxX) > center {
+			if at(im, x+1, y-1, maxX) > center {
 				bits |= 0x1 << 7 // NW
 			}
 
@@ -175,5 +189,11 @@ func (lbp *Lbp) Process(path string, regionSize int) []float64 {
 		offset++
 	}
 
-	return features
+	return features, err
+}
+
+// Convenience function for using 2-d indexing on a
+// 1-d array.
+func at(image []uint8, x int, y int, w int) uint8 {
+	return image[x+y*w]
 }
